a17ea23dc17879ec6ac7dbfc8c5d2bc0aaf54628
Sugi <developer.sugiono@gmail.com>
2020-05-29 01:45:58 +0700

    add player

--- src/player.ts
+++ src/player.ts
@@ -1,5 +1,34 @@
-import { Object3D } from "three";
+import { Body, Box, Vec3 } from 'cannon-es';
+import { BoxGeometry, Mesh, MeshPhongMaterial, Object3D, Vector3 } from 'three';
 
 export class Player extends Object3D {
 
+  body!: Body;
+
+  constructor() {
+    super();
+    this.setup();
+  }
+
+  setup(): void {
+    const mesh = new Mesh(
+      new BoxGeometry(.5, 1, .5),
+      new MeshPhongMaterial({color: 0xff0000})
+    );
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    this.add(mesh);
+
+    const shape = new Box(new Vec3(.25, .5, .25));
+    this.body = new Body({shape, mass: 1});
+    this.body.position.set(0, 5, 3);
+    this.body.fixedRotation = true;
+  }
+
+  update(): void {
+    const position = new Vector3();
+    position.fromArray(this.body.position.toArray());
+    this.position.copy(position);
+  }
 }
